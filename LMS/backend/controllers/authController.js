cfa5c3269398f55f36f6c676689d513d064d3362
Divyansh07-dev <divyanshsharma1972005@gmail.com>
2026-03-11 15:26:02 +0530

    Fix sameSite attribute value for cookies

--- LMS/backend/controllers/authController.js
+++ LMS/backend/controllers/authController.js
@@ -35,7 +35,7 @@ export const signUp=async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -61,7 +61,7 @@ export const login=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
@@ -77,7 +77,11 @@ export const login=async(req,res)=>{
 
 export const logOut = async(req,res)=>{
     try {
-        await res.clearCookie("token")
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        })
         return res.status(200).json({message:"logOut Successfully"})
     } catch (error) {
         return res.status(500).json({message:`logout Error ${error}`})
@@ -98,7 +102,7 @@ export const googleSignup = async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
